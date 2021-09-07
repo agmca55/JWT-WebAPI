7f940645efc5dd22842ac0566022b2fcaa20fd28
Anil Gautam <80522840+agmca55@users.noreply.github.com>
2021-09-07 18:24:37 +0530

    Update AuthenticationController.cs

--- JWT-WebAPI/Controllers/AuthenticationController.cs
+++ JWT-WebAPI/Controllers/AuthenticationController.cs
@@ -10,6 +10,7 @@ using JWT_WebAPI.Models;
 namespace JWT_WebAPI.Controllers
 {
    //this is branch 2 changes-new
+   //anil
     [Route("api/[controller]")]
     [ApiController]
     public class AuthenticationController : ControllerBase
